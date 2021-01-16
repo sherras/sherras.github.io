259798ba9ddaac68bc0b06fb12f0240daee37b6d
sherras <swa20014@byui.edu>
2021-01-15 17:27:59 -0700

    Fixed everything on the feedback

--- Assignment 01/script/assignment.js
+++ Assignment 01/script/assignment.js
@@ -1,4 +1,6 @@
 function datesTimes() {
     let dateNow = document.lastModified;
     document.getElementById("currentdate").innerHTML = dateNow;
-}
+    let newYear = new Date().getFullYear()
+    document.getElementById('year').innerHTML = newYear;
+}  
