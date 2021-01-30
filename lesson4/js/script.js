24393816521be9fb55f1eca14cc57879e5684d06
sherras <swa20014@byui.edu>
2021-01-30 15:41:47 -0700

    Almost done. Need help aligning header and nav

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,9 +1,42 @@
 function toggleMenu(){
     document.getElementById("firstNav").classList.toggle('close');
 }
-function datesTimes() {
+/*function datesTimes() {
     let dateNow = document.lastModified;
     document.getElementById("currentdate").innerHTML = dateNow;
+    
+}*/
+function datesTimes(){
+    let dayNames = 
+    [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let d = new Date();
+    let dayName = dayNames[d.getDay()];
+    let monthName = months[d.getMonth()];
+    let year = d.getFullYear();
+    let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+    document.getElementById("currentdate").innerHTML = fulldate;
     let newYear = new Date().getFullYear();
     document.getElementById('year').innerHTML = newYear;
 }
