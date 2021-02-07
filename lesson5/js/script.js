251047dc59d80e49a9151dfc109ea69109fcef07
sherras <swa20014@byui.edu>
2021-02-06 17:58:08 -0700

    preston 5 page

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -35,8 +35,27 @@ function datesTimes(){
     let dayName = dayNames[d.getDay()];
     let monthName = months[d.getMonth()];
     let year = d.getFullYear();
-    let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+    let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
     document.getElementById("currentdate").innerHTML = fulldate;
     let newYear = new Date().getFullYear();
     document.getElementById('year').innerHTML = newYear;
+}
+
+function banner(){
+    let dayNames = 
+    [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
+    let d = new Date();
+    let day = dayNames[d.getDay()];
+    if (day == "Saturday"){
+        alert("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
+    }
+    
 }
