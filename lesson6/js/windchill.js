82b165838ea47b39d29a3708401cc3bab2aaf92e
sherras <swa20014@byui.edu>
2021-02-16 17:17:20 -0700

    trying to fix windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,10 +1,9 @@
 function windChill(){
-    let t= parseFloat(document.getElementById("temp"));
-    let s = parseFloat(document.getElementById('speed'));
+    let t= parseFloat(document.getElementById("temp").innerHTML);
+    let s = parseFloat(document.getElementById('speed').innerHTML);
     if(t <= 50 && s > 3){
         let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
         let chill= f + "°F";
-        
     }
         else{
             chill= "N/A";
