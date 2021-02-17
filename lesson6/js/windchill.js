715af6ebcb7287a22631b66158c52219f3381ea4
sherras <swa20014@byui.edu>
2021-02-16 17:22:27 -0700

    more wind chill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,10 +3,10 @@ function windChill(){
     let s = parseFloat(document.getElementById('speed').innerHTML);
     if(t <= 50 && s > 3){
         let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
-        let chill= f + "°F";
+        var chill= f + "°F";
     }
         else{
-            chill= "N/A";
+         chill= "N/A";
         }
         document.getElementById('windC').innerHTML = chill;
 }
