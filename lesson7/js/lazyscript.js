fc210c22c8566cb127537d529bd58d3ebb2f8e20
sherras <swa20014@byui.edu>
2021-02-18 09:56:27 -0700

    fixed the lazy load, thank you Sis. Campbell!

--- lesson7/js/lazyscript.js
+++ lesson7/js/lazyscript.js
@@ -11,9 +11,7 @@ const loadImages = (image) => {
         image.removeAttribute('data-src');
     };
 };
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+
 if ('IntersectionObserver' in window){
 const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) =>{
