e92c40a4cdd3315db4a32811ff534216f6a418f3
sherras <swa20014@byui.edu>
2021-03-13 10:11:52 -0700

    weather api

--- lesson-10/js/forecast2.js
+++ lesson-10/js/forecast2.js
@@ -7,7 +7,13 @@ fetch(apiURL2)
    let day = 0;
    const dayofWeek =['Sun', 'Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat'];
   // reduce the list array to the five forecasts.
+  //filter jsObject.list.filter
+  //feed the filter a condition: it has to be 1800 dt_txt and see if it includes use includes(18:00)
 //loop through each of the forecast days
+const theFive = jsObject.list.filter(list.dt_txt == '18:00:00');
+thefive.foreach(x=>{
+
+})
     let d = new Date(jsObject.list[4].dt_txt)
 
     document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
