9f83ed51f52183a4a50799244f95194ec6699fe5
sherras <swa20014@byui.edu>
2021-03-15 13:56:25 -0600

    made the pages for lesson 11

---
+++ lesson-11/js/script.js
@@ -0,0 +1,53 @@
+function toggleMenu(){
+    document.getElementById("firstNav").classList.toggle('close');
+}
+/*function datesTimes() {
+    let dateNow = document.lastModified;
+    document.getElementById("currentdate").innerHTML = dateNow;
+    
+}*/
+function datesTimes(){
+    let dayNames = 
+    [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+    ];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let d = new Date();
+    let dayName = dayNames[d.getDay()];
+    let monthName = months[d.getMonth()];
+    let year = d.getFullYear();
+    let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+    document.getElementById("currentdate").innerHTML = fulldate;
+    let newYear = new Date().getFullYear();
+    document.getElementById('year').innerHTML = newYear;
+}
+
+function banner(){
+    let d = new Date();
+    let day = d.getDay();
+    if (day == 5){
+        document.getElementById("bannerFri").classList.toggle('hide');
+    }
+}
+WebFont.load({google: {families: ['Rubik']}});
+
+
